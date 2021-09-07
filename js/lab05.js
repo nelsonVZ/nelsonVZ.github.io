4698d65845985ccff99c9f0fe1eec35df64de741
Nelson Villanueva Zona <nvillanuevaz15_2@unc.edu.pe>
2021-09-06 21:48:15 -0500

    boton colorear v1.5.0

--- js/lab05.js
+++ js/lab05.js
@@ -3,6 +3,9 @@
     let btnExtraer = document.getElementById("boton-extraer");
     btnExtraer.onclick = extraer;
 
+    let btnColorear = document.getElementById("boton-colorear");
+    btnColorear.onclick = colorear;
+
     function extraer() {
         let nombre = document.getElementById("nombre-apellido").value.trim();
         let fecha = new Date(document.getElementById("fechaNacimiento").value);
@@ -37,6 +40,33 @@
         } else {
             document.getElementById("apPaterno").value = "";
             alert("Campo apellidos y nombres, campo fecha de nacimiento. Son abligatorios!!!");
+        }
+    }
+    let c = 0;
+
+    function colorear() {
+        if (c == 0) {
+            let colorAzul = document.getElementsByClassName("fondo-azul");
+            let colorAnaranjado = document.getElementsByClassName("fondo-anaranjado");
+
+            while (colorAzul.length != 0) {
+                document.getElementsByClassName("fondo-azul")[0].className = "fondo-anaranjado-nuevo";
+
+            }
+            while (colorAnaranjado.length != 0) {
+                document.getElementsByClassName("fondo-anaranjado")[0].className = "fondo-azul-nuevo";
+            }
+            c = 1;
+        } else {
+            let colorAzul = document.getElementsByClassName("fondo-azul-nuevo");
+            let colorAnaranjado = document.getElementsByClassName("fondo-anaranjado-nuevo");
+            while (colorAzul.length != 0) {
+                document.getElementsByClassName("fondo-azul-nuevo")[0].className = "fondo-anaranjado";
+            }
+            while (colorAnaranjado.length != 0) {
+                document.getElementsByClassName("fondo-anaranjado-nuevo")[0].className = "fondo-azul";
+            }
+            c = 0;
         }
     }
 })();
